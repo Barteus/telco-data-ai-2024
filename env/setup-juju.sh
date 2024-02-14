0efd90cf3182614d6c8a5441a6c9c4813828cbd6
Barteus <b.poniecki.klotz@gmail.com>
2024-02-14 15:52:16 +0000

    vm setup & opensearch deployment

--- env/setup-juju.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo snap install juju --channel 3.1/stable
-mkdir -p ~/.local/share/juju
---
+++ env/setup-vm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo snap install juju --channel 3.1/stable
+mkdir -p ~/.local/share/juju
+
+sudo snap install kubectl --classic
+mkdir -p ~/.kube
+
+sudo snap install microk8s --classic --channel=1.28/stable
+sudo usermod -a -G microk8s ubuntu
+sudo chown -R ubuntu ~/.kube
+newgrp microk8s # or restart the console
+
+microk8s status
+microk8s enable dns hostpath-storage ingress metallb:10.64.140.43-10.64.140.49 rbac
+kubectl get po -A
+
+
+#missing switch from sudo to the ubuntu user for microk8s
+
+juju bootstrap localhost
+juju add-k8s mk8s --cluster-name=microk8s-cluster
+
+
