4876a03f23354b370d86eac6649b8a577931ea32
Bartłomiej Poniecki-Klotz <Barteus@users.noreply.github.com>
2024-03-27 20:50:41 +0100

    Update deploy-cos.sh

--- env/deploy-cos.sh
+++ env/deploy-cos.sh
@@ -7,13 +7,13 @@ juju deploy cos-lite --model cos \
   --overlay ./cos/offers-overlay.yaml \
   --overlay ./cos/storage-small-overlay.yaml
 
-juju offer cos.traefik:ingress
+juju offer cos.traefik:ingress --model cos
 
-juju run traefik/0 show-proxied-endpoints --format=yaml \
+juju run traefik/0 show-proxied-endpoints --format=yaml --model cos \
   | yq '."traefik/0".results."proxied-endpoints"' \
   | jq
 
-juju show-unit catalogue/0 | grep url
+juju show-unit catalogue/0 --model cos | grep url
 
 #User admin
 juju run grafana/leader get-admin-password --model cos
